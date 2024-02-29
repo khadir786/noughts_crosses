eaaa7288b9e7ae4601ba8f1691341bfee3a0136a
Khadir Abdul <khadir786.ka@googlemail.com>
2024-02-29 17:58:52 +0000

    Refactored Board component to use an array of class names for tiles and a function to generate Tile components.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,62 +2,43 @@ import Strike from "./Strike";
 import Tile from "./Tile";
 
 export default function Board({ tiles, onTileClick, currentPlayer, strikeType }) {
+  // Array to hold classNames corresponding to each tile position
+  const tileClassNames = [
+    ["right-border", "bottom-border"], // Tile 0
+    ["right-border", "bottom-border"], // Tile 1
+    ["bottom-border"],                 // Tile 2
+    ["right-border", "bottom-border"], // Tile 3
+    ["right-border", "bottom-border"], // Tile 4
+    ["bottom-border"],                 // Tile 5
+    ["right-border"],                  // Tile 6
+    ["right-border"],                  // Tile 7
+    []                                 // Tile 8
+  ];
+
+  // Function to render a Tile component based on index and classNames
+  const renderTile = (index) => (
+    <Tile
+      key={index}
+      onClick={() => onTileClick(index)}
+      value={tiles[index]}
+      className={tileClassNames[index].join(" ")}
+      currentPlayer={currentPlayer}
+    />
+  );
+
+  // Generate Tile components using renderTile function
+  const generateTiles = () => {
+    const tileComponents = [];
+    for (let i = 0; i < 9; i++) {
+      tileComponents.push(renderTile(i));
+    }
+    return tileComponents;
+  };
+
   return (
     <div className="BoardContainer">
-      <Tile
-        onClick={() => onTileClick(0)}
-        value={tiles[0]}
-        className="right-border bottom-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(1)}
-        value={tiles[1]}
-        className="right-border bottom-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(2)}
-        value={tiles[2]}
-        currentPlayer={currentPlayer}
-        className="bottom-border"
-      />
-      <Tile
-        onClick={() => onTileClick(3)}
-        value={tiles[3]}
-        className="right-border bottom-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(4)}
-        value={tiles[4]}
-        className="right-border bottom-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(5)}
-        value={tiles[5]}
-        className="bottom-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(6)}
-        value={tiles[6]}
-        className="right-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(7)}
-        value={tiles[7]}
-        className="right-border"
-        currentPlayer={currentPlayer}
-      />
-      <Tile
-        onClick={() => onTileClick(8)}
-        value={tiles[8]}
-        currentPlayer={currentPlayer}
-      />
-      <Strike strikeType={strikeType} currentPlayer={currentPlayer}/>
+      {generateTiles()}
+      <Strike strikeType={strikeType} currentPlayer={currentPlayer} />
     </div>
   );
 }
